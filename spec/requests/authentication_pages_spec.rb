9eb9eed9416d1abfb38862d265a41df6addcb197
zat <giacomokavanagh@hotmail.com>
2012-10-22 18:48:06 +0100

    checking authentication pages spec appears

---
+++ spec/requests/authentication_pages_spec.rb
@@ -0,0 +1,47 @@
+require 'spec_helper'
+
+describe "Authentication" do
+
+  subject { page }
+
+  describe "signin page" do
+    before { visit signin_path }
+
+    it { should have_selector('h1',    text: 'Sign in') }
+    it { should have_selector('title', text: 'Sign in') }
+  end
+
+  describe "signin" do
+    before { visit signin_path }
+
+    describe "with invalid information" do
+      before { click_button "Sign in" }
+
+      it { should have_selector('title', text: 'Sign in') }
+      it { should have_error_message }
+
+      describe "after visiting another page" do
+        before { click_link "Home" }
+        it { should_not have_error_message }
+      end
+    end
+
+    describe "with valid information" do
+      let(:user) { FactoryGirl.create(:user) }
+      before { sign_in user }
+
+      it { should have_selector('title', text: user.name) }
+      it { should have_link('Profile',     href: user_path(user)) }
+      it { should have_link('Sign out',    href: signout_path) }
+      it { should have_link('Settings',    href: edit_user_path(user)) }
+      it { should have_link('Users',       href: users_path) }
+      it { should_not have_link('Sign in', href: signin_path) }
+
+      describe "followed by signout" do
+        before { click_link "Sign out" }
+        it { should have_link('Sign in') }
+      end
+    end
+  end
+end 
+
