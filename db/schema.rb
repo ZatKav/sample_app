5480a15c4626e75992d4f6accc31ed982c7885d1
zat <giacomokavanagh@hotmail.com>
2012-10-29 23:36:17 +0000

    changes!

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121029120540) do
+ActiveRecord::Schema.define(:version => 20121029230118) do
+
+  create_table "microposts", :force => true do |t|
+    t.string   "content"
+    t.integer  "user_id"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  add_index "microposts", ["user_id", "created_at"], :name => "index_microposts_on_user_id_and_created_at"
 
   create_table "users", :force => true do |t|
     t.string   "name"
@@ -20,8 +29,10 @@ ActiveRecord::Schema.define(:version => 20121029120540) do
     t.datetime "updated_at",                         :null => false
     t.string   "password_digest"
     t.boolean  "admin",           :default => false
+    t.string   "remember_token"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
+  add_index "users", ["remember_token"], :name => "index_users_on_remember_token"
 
 end
---
+++ spec/models/micropost_spec.rb
@@ -0,0 +1,16 @@
+require 'spec_helper'
+
+describe Micropost do
+
+  let(:user) { FactoryGirl.create(:user) }
+  before do
+    # This code is wrong!
+    @micropost = Micropost.new(content: "Lorem ipsum", user_id: user.id)
+  end
+
+  subject { @micropost }
+
+  it { should respond_to(:content) }
+  it { should respond_to(:user_id) }
+end
+
